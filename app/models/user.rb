f255390249deadce6dff9b6ee3d0629f421db162
eisacke <emily@isacke.com>
2016-03-19 17:27:37 +0000

    Added carrierwave and geocoder gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :courses
+  mount_uploader :image, ProfileImageUploader
+  geocoded_by :location
+  after_validation :geocode
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
